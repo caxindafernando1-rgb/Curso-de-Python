8f3de960af8413bc2d75a6284e1e1a0276fffaae
Fernando Caxinda <caxindafernando1@gmail.com>
2026-04-01 17:04:28 +0100

    exercício 110

--- exercico109/moeda.py
+++ exercico109/moeda.py
@@ -1,9 +1,9 @@
-def aumento(preço=0, taxa=0, formato = False):
+def aumento(preço=0, taxa=10, formato = False):
       resp = preço + (preço * (taxa/100))
       return resp if formato is False else moeda(resp)
 
 
-def diminuição(preço=0, taxa=0, formato=False):
+def diminuição(preço=0, taxa=10, formato=False):
       resp = preço - (preço * (taxa/100))
       return resp if formato is False else moeda(resp)
 
@@ -19,4 +19,18 @@ def matade(preço=0, Formato=False):
 
 
 def moeda(preço=0, moeda= 'Kzs'):
-      return f'{moeda}{preço:.2f}'.replace('.', ',')
+      return f'{preço:.2f}{moeda}'.replace('.', ',')
+
+
+def resumo(preço=0, taxaa=10, taxar=-5):
+      print('-' * 30)
+      print('-' * 30)
+      print(f'Analise do preço {moeda(preço)}'.center(30))
+      print('~' * 30)
+      print(f'Aumento é de \t{aumento(preço, taxaa, True)}')
+      print(f'Sem o aumento \t{diminuição(preço, taxar, True)}')
+      print(f'O dobro é \t{dobro(preço, True)}')
+      print(f'A metade é \t{matade(preço, True)}')
+      print('-' * 30)
+      print('-' * 30)
+
