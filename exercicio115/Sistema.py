48af9cc698f32f624ee2769ffbb44d6a6d97398c
Fernando Caxinda <caxindafernando1@gmail.com>
2026-04-02 17:40:18 +0100

    exerccio 115b feito

--- exercicio115/Sistema.py
+++ exercicio115/Sistema.py
@@ -1,11 +1,16 @@
 from dodo import *
 from time import sleep
+from arquivo import *
 
+arq = 'cursoEmVideo.txt'
+
+if not arquivoExiste(arq):
+      creiarArquivo(arq)
 
 while True:
       resposta = menu(['Criar Arquivo', 'Cadastrar Pessoas', 'Sair do Programa'])
       if resposta == 1:
-            cabeçalho('\033[1;35m Opção 1 selecionada\033[m')
+            cabeçalho(f'\033{lerArquivo(arq)}\033[m')
       elif resposta == 2:
             cabeçalho('\033[1;35m Opção 2 selecionada\033[m')
       elif resposta == 3:
---
+++ exercicio115/arquivo.py
@@ -0,0 +1,29 @@
+from dodo import *
+
+def arquivoExiste(nome):
+      try:
+            a = open(nome, 'rt')
+            a.close()
+      except FileNotFoundError:
+            return False
+      else:
+            return True
+      
+def creiarArquivo(nome):
+      try:
+            a = open(nome, 'wt+')
+            a.close
+      except:
+            print('Houve um ERRO na criação do arquivo!')
+      else:
+            print(f'Arquivo {nome} Criado com sucesso!')
+
+
+def lerArquivo(nome):
+      try:
+            a = open(nome, 'rt')
+      except:
+            print('Erro ao ler Arquivo!')
+      else:
+            cabeçalho('== PESSOAS CADASTRADAS ==')
+            print(a.read())
