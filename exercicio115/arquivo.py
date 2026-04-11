82e45be72f0d9bd6d4b4fd6483ef0213b972fd7a
Fernando Caxinda <caxindafernando1@gmail.com>
2026-04-12 00:17:59 +0100

    reg

--- exercicio115/arquivo.py
+++ exercicio115/arquivo.py
@@ -25,7 +25,7 @@ def lerArquivo(nome):
       except:
             print('Erro ao ler Arquivo!')
       else:
-            cabeçalho('== PESSOAS CADASTRADAS ==')
+            cabeçalho('== Regitrar Pagamentos ==')
             for linha in a:
                   dodo = linha.split(';')
                   dodo[1] = dodo[1].replace('\n', '')
