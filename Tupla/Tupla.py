73904d5ffb136034ff863a9ea88b4d1381d8e072
Fernando Caxinda <caxindafernando1@gmail.com>
2026-09-11 08:14:21 +0100

    Update Tupla.py

--- Tupla/Tupla.py
+++ Tupla/Tupla.py
@@ -8,6 +8,3 @@ print(coordenadas[0])
 print(coordenadas[1])
 print(coordenadas[2])
 
-#ver totas as pastas do repositorio: Get-ChildItem
-#abrir pasta tupla.py:  Get-ChildItem -Recurse -Filter tupla.py
-#executar o programa em python:  py .\Tupla\Tupla.py
